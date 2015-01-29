67b25e7128d2e20ad01845beae7415b22fc78fc1
mannec <mannecollins@hotmail.com>
2015-01-29 20:37:32 +0100

    Småfix
    Fixade lite strings n shiet

--- Labb1.py
+++ Labb1.py
@@ -1,14 +1,10 @@
 import time
+
 from operator import attrgetter
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/master
 
 def readFile():
-    """Läser in filen geodataSW2 och skapar objekt med egenskaper motsvarande fem parametrar"""
-    with open("geodataSW2.txt", "r", encoding="utf-8") as f:
+    """Läser in filen geodataCH och skapar objekt med egenskaper motsvarande fem parametrar"""
+    with open("geodataCH.txt", "r", encoding="utf-8") as f:
         array_list = []
         a=1
         for line in f:
@@ -16,43 +12,41 @@ def readFile():
 
                     if a == 1:
                         name = line
-                        a=a+1
+                        a+=1
                     elif a == 2:
                         description = line
-                        a=a+1
+                        a+=1
                     elif a == 3:
                         latitude = line
-                        a=a+1
+                        a+=1
                     elif a == 4:
                         longitude = line
-                        a=a+1
+                        a+=1
                     elif a == 5:
                         date = line
                         array_list.append(Place(name, description, latitude, longitude, date))
                         a=1
-
+               
     return array_list
 
 def findPlace(intext, array):
-    """Går igenom listan med objekt och hittar objektet med namn motsvarande intext"""
+    """Går igenom listan med objekt och hittar objektet med namn motsvarande intext, och tar tid på sökningen"""
     time_start = time.time() #Tid vid start av "sökfunktionen"
     for x in array:
         if x.getName().lower() == intext.lower(): #Om namnet för specifikt objekt matchar användarens input, kör __str__-funktion för sagda objekt.
             print(x)
     time_end = time.time() #Tid då "sökfunktionen" är klar
     time_passed = time_end-time_start #Åtgången tid
-    print(time_passed)
+    print("Time for search:" + str(time_passed))
 
-<<<<<<< HEAD
 
-=======
+
 def southernPlace(array_list):
     '''Sorterar listan med objekt i storleksordning vad gäller attributet longitud, från minst till störst och skriver ut den sydligaste'''
     all_places_sorted = sorted(array_list, key=attrgetter('longitude')) 
-    print(all_places_sorted[0])
+    print("However, the most southern place is " + str(all_places_sorted[0]))
     
-            
->>>>>>> origin/master
+
 class Place:
     '''Skapar en klass med attribut för namn, beskrivning, latitud, longitud och datum'''
 
@@ -65,31 +59,23 @@ class Place:
 
 
     def getName(self):
-        '''Returnerar namnet pÃ¥ platsen'''
+        '''Returnerar namnet på platsen'''
         return self.name
 
     def getLongitude(self):
-        '''Returnerar longitud fÃ¶r platsen'''
+        '''Returnerar longitud för platsen'''
         return int(self.longitude)
-
-
+        
+        
     def __str__(self):
-        return "This is " +str(self.name)+ ". Welcome! This is a " +str(self.description)+ " place."
-
-def findSouthernSpot(array_list):
-    test = array_list.sort(key = attrgetter('longitude'))
-    print(test)
-
+        """Returnerar en string av argumentet name och description"""
+        return  str(self.name)+ ", often refferd to as " +str(self.description)
 
 def main():
-
     all_places = readFile()
     intext = input("Skriv in en geografisk plats: ") #Tar emot indata som sedan blir ett ställe som matchas mot listan med object
     findPlace(intext, all_places)
-<<<<<<< HEAD
-    findSouthernSpot(all_places)
-=======
+
     southernPlace(all_places)
 
->>>>>>> origin/master
 main()

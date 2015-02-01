7cb4639401ee4ab45b4571781dc24ab0d16cf0d8
mannec <mannecollins@hotmail.com>
2015-02-01 17:26:18 +0100

    Kommentarer
    Ökat anänvdarvänligheten med kommentarer och strängar

--- Labb1.py
+++ Labb1.py
@@ -4,7 +4,7 @@ from operator import attrgetter
 
 def readFile():
     """Läser in filen geodataCH och skapar objekt med egenskaper motsvarande fem parametrar"""
-    with open("geodataCH.txt", "r", encoding="utf-8") as f:
+    with open("geodataSW2.txt", "r", encoding="utf-8") as f:
         array_list = []
         a=1
         for line in f:
@@ -27,22 +27,32 @@ def readFile():
                         array_list.append(Place(name, description, latitude, longitude, date))
                         a=1
                
-    return array_list
+    return array_list #returnerar en array med platser lagrade som objekt från klassen Place
 
 def findPlace(intext, array):
-    """Går igenom listan med objekt och hittar objektet med namn motsvarande intext, och tar tid på sökningen"""
+    """Tar emot input från användaren och en lista med objekt, hittar objektet med namn motsvarande intext, och tar tid på sökningen."""
     time_start = time.time() #Tid vid start av "sökfunktionen"
     for x in array:
         if x.getName().lower() == intext.lower(): #Om namnet för specifikt objekt matchar användarens input, kör __str__-funktion för sagda objekt.
             print(x)
+
+
+       #else:
+            #print(intext +"could not be found, try again")
+            
+            
+            
     time_end = time.time() #Tid då "sökfunktionen" är klar
     time_passed = time_end-time_start #Åtgången tid
     print("Time for search:" + str(time_passed))
 
 
+def userData():
+    """Tar emot indata som sedan blir ett ställe som matchas mot listan med object"""
+    return input("Welcome to Locator! \n You can search for a location and get information about it.\n Type in a geografic location: ") 
 
 def southernPlace(array_list):
-    '''Sorterar listan med objekt i storleksordning vad gäller attributet longitud, från minst till störst och skriver ut den sydligaste'''
+    '''Sorterar en lista med objekt i storleksordning vad gäller attributet longitud, från minst till störst och skriver ut den sydligaste'''
     all_places_sorted = sorted(array_list, key=attrgetter('longitude')) 
     print("However, the most southern place is " + str(all_places_sorted[0]))
     
@@ -72,8 +82,9 @@ class Place:
         return  str(self.name)+ ", often refferd to as " +str(self.description)
 
 def main():
+    """mainfunktionen för programmet"""
     all_places = readFile()
-    intext = input("Skriv in en geografisk plats: ") #Tar emot indata som sedan blir ett ställe som matchas mot listan med object
+    intext = userData()
     findPlace(intext, all_places)
 
     southernPlace(all_places)

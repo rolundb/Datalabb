bbd071051316f97374b2590c2dd431b69dbc88ed
rolundb <robin.s.lundberg@gmail.com>
2015-02-02 18:09:19 +0100

    A-labb innan dubbelfiländring

--- Labb1.py
+++ Labb1.py
@@ -1,5 +1,4 @@
 import time
-import sys
 import os
 from operator import attrgetter
 
@@ -12,27 +11,27 @@ def readFile():
     with open("geodataSW.txt", "r", encoding="utf-8") as f:
         the_array_list = []
         the_reset = 0
-        a= the_reset
+        i= the_reset
         for line in f:
             if line.strip() != "" and not line.startswith("#"):
-                a+=1
+                i+=1
                 
-                if a == 1:
+                if i == 1:
                     name = line
 
-                elif a == 2:
+                elif i == 2:
                     description = line
 
-                elif a == 3:
+                elif i == 3:
                     latitude = line
 
-                elif a == 4:
+                elif i == 4:
                     longitude = line
 
-                elif a == 5:
+                elif i == 5:
                     date = line
                     the_array_list.append(Place(name, description, latitude, longitude, date))
-                    a = the_reset
+                    i = the_reset
                
     return the_array_list
 
@@ -45,15 +44,15 @@ def readFile():
 
 def findPlace(an_intext, an_array):
     the_time_start = time.time()
-    for x in an_array:
-        if x.getName().lower() == an_intext.lower(): 
-            print(x)
+    for an_object in an_array:
+        if an_object.getName().lower() == an_intext.lower(): 
+            print(an_object)
             the_time_end = time.time()
             the_time_passed = the_time_end-the_time_start 
             print("Time for search:" + str(the_time_passed)+ " seconds.")
             return
 
-    print("\n" +an_intext +" could not be found. Please try again.\n\n")
+    print("\n" + an_intext +" could not be found. Please restart the program to try again.\n\n")
     os._exit(0)
 
     
@@ -66,7 +65,7 @@ def findPlace(an_intext, an_array):
           
 def southernPlace(an_array):
     all_places_sorted = sorted(an_array, key=attrgetter('longitude')) 
-    print("However, the most southern place is " + str(all_places_sorted[0]))
+    print("However, the most southern place is " + str(all_places_sorted[0]) + ". If you want to search for another location, please restart the program.")
     
 
 class Place:
@@ -95,7 +94,7 @@ class Place:
 
 def main():
     all_places = readFile()
-    intext = input("Welcome to Locator! \n You can search for a location and get information about it.\n Type in a geografic location: ")
+    intext = input("Welcome to Locator! \n You can search for a location and get information about it. As a bonus, we'll throw in the name of the most southern place.\n Type in a geografic location: ")
     findPlace(intext, all_places)
     southernPlace(all_places)
 
